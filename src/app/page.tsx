aad2c01c2e9f2f9d59c87a342f2693ba65039e78
Mitchell Christ <xzmaru@gmail.com>
2025-06-29 00:13:59 -0700

    add ver num

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getAccessToken } from '@/lib/yahoo/auth'
 import { redirect } from 'next/navigation'
 import SignIn from '@/ui/yahoo/sign-in'
+import { version } from '@@/package.json'
 
 export default async function Home() {
 	const accessToken = await getAccessToken()
@@ -10,8 +11,12 @@ export default async function Home() {
 	}
 
 	return (
-		<main className="px-ch grid place-content-center">
+		<main className="px-ch grid place-content-center gap-[.5ch] text-center">
 			<SignIn />
+
+			<small className="text-foreground/50 text-[x-small]">
+				Version {version}
+			</small>
 		</main>
 	)
 }
